92cd494b5bc97b91b0541d76cda778ccea788288
harrywilderr <arthurharrywilder@gmail.com>
2025-07-26 22:20:58 -0500

    exists

--- netlify/functions/fetchCredits.js
+++ netlify/functions/fetchCredits.js
@@ -27,7 +27,7 @@ export async function handler(event) {
       };
     }
 
-    const googleScriptUrl = `https://script.google.com/macros/s/AKfycbxvg5dnPr_htALThKKDK3eTgQJHQDLMKida3pNB48Z-3YuGBXsI89IoXVTz9J8cK2WrCw/exec?email=${encodeURIComponent(email)}`;
+    const googleScriptUrl = `https://script.google.com/macros/s/AKfycbyoGfjkb-E39JpKLKL-9clOCjQz032ncWRcH9pgYgRUYRhM2rVBhMe51PzFaERVdZ0ZWQ/exec?email=${encodeURIComponent(email)}`;
 
     try {
       const response = await fetch(googleScriptUrl);
@@ -74,7 +74,7 @@ export async function handler(event) {
         };
       }
 
-      const googleScriptUrl = `https://script.google.com/macros/s/AKfycbxvg5dnPr_htALThKKDK3eTgQJHQDLMKida3pNB48Z-3YuGBXsI89IoXVTz9J8cK2WrCw/exec?email=${encodeURIComponent(email)}`;
+      const googleScriptUrl = `https://script.google.com/macros/s/AKfycbyoGfjkb-E39JpKLKL-9clOCjQz032ncWRcH9pgYgRUYRhM2rVBhMe51PzFaERVdZ0ZWQ/exec?email=${encodeURIComponent(email)}`;
       const response = await fetch(googleScriptUrl);
       const data = await response.json();
 
